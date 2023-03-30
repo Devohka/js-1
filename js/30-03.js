04302470c2d640fdcb85cf6c26e357b46b77f7cf
Devohka <110058157+Devohka@users.noreply.github.com>
2023-03-30 18:11:15 +0300

    js-2

---
+++ js/30-03.js
@@ -0,0 +1,24 @@
+const str = "Привіт";
+console.log(typeof str)
+
+const num = 123;
+console.log(typeof num);
+
+const flag = true;
+console.log(typeof flag);
+
+const txt = "true";
+console.log(typeof txt);
+
+const infoBlock = prompt("ім’я користувача");
+console.log(infoBlock);
+
+let my = 13;
+let friend = 13;
+let result = my + friend;
+alert(result);
+
+let message = prompt("Я навчаюся в GoITeens в групі:");
+console.log(message);
+let group = "FE1y_1 менші";
+console.log(group);
